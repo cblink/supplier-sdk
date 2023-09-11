cfdf67eb3692a0b0a6919a33df18461913a3e5bb
0daynick <me@xieying.vip>
2023-09-11 18:37:04 +0800

    fix: 修复缺陷

--- src/Kernel/AccessToken.php
+++ src/Kernel/AccessToken.php
@@ -2,9 +2,11 @@
 
 namespace Cblink\Supplier\Kernel;
 
+use Psr\Http\Message\RequestInterface;
+
 class AccessToken extends \Cblink\Service\Foundation\AccessToken
 {
-    public function getToken()
+    public function getToken(RequestInterface $request = null, array $requestOptions = [])
     {
         return $this->app->config->get('api_token');
     }
